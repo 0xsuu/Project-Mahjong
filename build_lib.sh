e964b704ba59239cfcc4e616af5a995d17825837
al1ensuu <462993145@qq.com>
2016-10-22 16:43:03 +0100

    Constructed basic building system
    Signed-off-by: al1ensuu <462993145@qq.com>

---
+++ build_lib.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+BASEDIR=$(dirname "$0")
+
+# Go to project directory.
+cd "$BASEDIR"/../
+
+if [ ! -d ./Project-Mahjong ]; then
+    echo "Project name must be Project-Mahjong!"
+    exit -1
+fi
+
+# Create build folder.
+mkdir -p ./build_mahjong
+cd ./build_mahjong
+
+# Generate Xcode project
+if [ "$1" == "xcode" ]; then
+	mkdir -p xcode
+    cd xcode
+    cmake -G Xcode ../../Project-Mahjong/mahjong_lib/
+    open Mahjong-lib.xcodeproj
+    exit 0
+fi
+
+# Run test.
+if [ "$1" == "test" ]; then
+    cmake ../Project-Mahjong/mahjong_lib/ -DBUILD_TEST="YES"
+    make
+    ../libma_gtest/libma_test
+    exit 0
+fi
+
+cmake ../Project-Mahjong/mahjong_lib/ -DBUILD_TEST=""
+make
+
