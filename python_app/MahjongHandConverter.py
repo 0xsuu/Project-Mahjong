8e0b9f20402b28e1c3db512cd0d250f2e8387af5
Hao <hs1145@york.ac.uk>
2017-04-04 10:37:03 +0100

    Change to one hot for converter as well.
    Signed-off-by: Hao <hs1145@york.ac.uk>

--- python_app/MahjongHandConverter.py
+++ python_app/MahjongHandConverter.py
@@ -37,9 +37,16 @@ def toStringHand(hand):
 def expandHandToCSV(byteHand):
     retHand = []
     for i in byteHand:
-        retHand += list(bin(i)[2:].zfill(8))
+        # Convert to onehot encoding.
+        converted_hand = 0
+        converted_hand = 1 << (2 - ((i & 0b11000000) >> 6))
+        converted_hand <<= 13
+        converted_hand |= 1 << (3 - ((i & 0b110000) >> 4) + 9)
+        converted_hand |= 1 << (9 - (i & 0b1111))
+        retHand += list(bin(converted_hand)[2:].zfill(16))
     return retHand
 
 def transformCSVHandToCNNMatrix(csvHand):
     csvHand = np.array([csvHand])
     return csvHand.reshape(csvHand.shape[0], 1, 14, 8)
+
