b6b3b6cc1c896850710733268608c1d8493afb55
Hao <hs1145@york.ac.uk>
2017-04-27 10:58:34 +0100

    Move dqn cartpole under test folder.
    Signed-off-by: Hao <hs1145@york.ac.uk>

--- python_app/rl_algorithms/test/test_cartpole.py
+++ python_app/rl_algorithms/test/test_cartpole.py
@@ -14,10 +14,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import sys
-
-sys.path.append("../")
-
 from dqn_cartpole import *
 
 import gym
