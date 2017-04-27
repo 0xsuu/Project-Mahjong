60b7db43df940ef4a07950dffe1ff7b50ca9eee9
Hao <hs1145@york.ac.uk>
2017-04-27 16:44:08 +0100

    Add licenses.
    Signed-off-by: Hao <hs1145@york.ac.uk>

--- python_app/supervised_learning/predict_cnn.py
+++ python_app/supervised_learning/predict_cnn.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+#  Copyright 2017 Project Mahjong. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import sys
 
 from keras.models import load_model
@@ -19,4 +33,3 @@ proba = model.predict_proba(inputs)[0]
 p_class = int(model.predict_classes(inputs)[0])
 print(proba)
 print("Choose NO." + str(p_class) + " - " + hand.split(" ")[p_class] + " with probability: " + str(proba[p_class] * 100) + "%")
-
