2047f6811ef3e2aba6c6586172155f5d09e640b8
Hao <hs1145@york.ac.uk>
2017-11-29 23:57:05 +0000

    Add feature input to the hand.
    Signed-off-by: Hao <hs1145@york.ac.uk>

--- python_app/tiny_mahjong/play.py
+++ python_app/tiny_mahjong/play.py
@@ -30,12 +30,12 @@ def main():
     player1 = UserInputPlayer("Smart Human", log_game_state=True)
     player2 = QPlayer("Q BOT 1", EVAL)
     # player3 = DQNPlayer("DQN BOT TRAIN", EVAL)
-    # player4 = FullDQNPlayer("Full DQN BOT SELF_PLAY", SELF_PLAY)
+    # player4 = FullDQNPlayer("Full DQN BOT SELF_PLAY", SELF_PLAY, log_game_state=True)
     player5 = GreedyPlayer("Greedy BOT")
-    player6 = FullDQNPlayer("Full DQN BOT", TRAIN, evaluate=False, log_game_state=True)
+    player6 = FullDQNPlayer("Full DQN BOT", TRAIN, evaluate=True, log_game_state=True)
     player7 = SafetyFirstPlayer("Safety First Bot", EVAL, log_game_state=True)
 
-    game = Game(10000000, [player5, player6], win_on_discard=True, disclose_all=False)
+    game = Game(10000000, [player6, player5], win_on_discard=True, disclose_all=False)
     game.play()
 
 
